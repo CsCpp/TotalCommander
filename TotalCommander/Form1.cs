bc26b6f1bbd1dbd26851d4d5adc088eccfadc0b2
CppKotlin <cscppkotlin@gmail.com>
2024-04-21 12:06:54 +0300

    Добавьте файлы проекта.

---
+++ TotalCommander/Form1.cs
@@ -0,0 +1,45 @@
+namespace TotalCommander
+{
+    public partial class Form1 : Form
+    {
+        public void AddAdress()
+        {
+            listBox1.Items.Clear();
+            DirectoryInfo dir = new DirectoryInfo(textBox1.Text);
+            DirectoryInfo[] dirs = dir.GetDirectories();
+            foreach (var dirInfo in dirs)
+            {
+                listBox1.Items.Add(dirInfo);
+            }
+            FileInfo[] files = dir.GetFiles();
+            foreach (var file in files)
+            {
+                listBox1.Items.Add(file);
+            }
+
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+    
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text=="") textBox1.Text = "C:\\";
+            AddAdress();
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (textBox1.Text == "") textBox1.Text = "C:\\";
+                AddAdress();
+            }
+        }
+
+
+
+    }
+}
